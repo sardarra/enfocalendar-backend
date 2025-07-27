1a1cde3c5ed1f8f32a67e4a1e31c1f70b3455be8
sardarra <92699150+sardarra@users.noreply.github.com>
2025-07-27 00:25:31 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+// Key: mongodb+srv://sardarrah:T24i221N@calendar.x4ubwof.mongodb.net/calendar?retryWrites=true&w=majority&appName=calendar
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
